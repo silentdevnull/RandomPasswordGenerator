73d744e37ce5dab3fe0d508fed80f877fe94c240
silentdevnull <silentdevnull@protonmail.com>
2021-07-16 19:44:50 -0700

    Print the random password to screen

--- src/RandomPasswordGenerator/Program.cs
+++ src/RandomPasswordGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SilentDevNull.Utilities.Security;
 
 namespace RandomPasswordGenerator
 {
@@ -6,7 +7,45 @@ namespace RandomPasswordGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("Random Length");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword()}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Password Length 10");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword(10)}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword(10)}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword(10)}");
+            Console.WriteLine();
+            Console.WriteLine("Password length 15");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword(15)}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword(15)}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword(15)}");
+            Console.WriteLine();
+            Console.WriteLine("Password length 20");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword(20)}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword(20)}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword(20)}");
+            Console.WriteLine();
+            Console.WriteLine("Password length 25");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword(25)}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword(25)}");
+            Console.WriteLine($"{RandomPassword.CreateRandomPassword(25)}");
+            Console.WriteLine();
         }
     }
 }
